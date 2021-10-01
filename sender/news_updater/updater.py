231879c60205cf7bb67a001f23d7096d487d814a
hanjak <hanja.kosova@gmail.com>
2021-10-01 09:45:40 +0200

    changed settings

--- sender/news_updater/updater.py
+++ sender/news_updater/updater.py
@@ -3,5 +3,5 @@ from news_updater import news_downloader
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(news_downloader.check_webnews, 'interval', minutes=1)
+    scheduler.add_job(news_downloader.check_webnews, 'interval', minutes=300)
     scheduler.start()
