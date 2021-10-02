a93229a40cc9c0826abb1d93bdabeaa2f789f376
hanjak <hanja.kosova@gmail.com>
2021-10-02 10:01:07 +0200

    set environ variables

--- sender/sender/settings.py
+++ sender/sender/settings.py
@@ -136,7 +136,7 @@ DEFAULT_FROM_EMAIL = 'pyhanja@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'emailpyhanja@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
