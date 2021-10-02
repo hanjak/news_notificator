3ecf52ea0a143c50175384d0bb8ce3f17de113d9
hanjak <hanja.kosova@gmail.com>
2021-10-02 09:56:10 +0200

    requirements.txt

--- sender/sender/settings.py
+++ sender/sender/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-)eii#yx43a4m(#%o(2dv@9)3n$nnnsqn1dpo-iy=x!c0#s&l$7'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
